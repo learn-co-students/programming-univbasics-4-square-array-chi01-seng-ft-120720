3d619dd6c4ae605ba094573a9c1ba64c5b4ee5e3
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-20 19:29:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+
+def square_array(numbers)
+  new_numbers= []
+  numbers.length.times do |index|
+    new_numbers.push(numbers[index]**2)
+  end
+  new_numbers
 end
+
+#square_array([1,2,3,4,5])
