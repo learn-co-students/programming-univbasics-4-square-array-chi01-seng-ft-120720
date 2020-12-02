0dc141da5c0cc3660a52e823da030d8320103103
James Sapp <jameseisapp@gmail.com>
2020-12-02 20:14:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,12 +5,10 @@ def square_array(array)
     new_array = []
     counter = 0 
     
-    while counter < array.length do 
-      num = array[counter]
-      squared = num ** 2
-      new_array.push(squared)
-      counter += 1   
-    end
-  
+    array.length.times do |index|
+      binding.pry 
+      new_array.push(array[index] ** 2)
+      counter += 1 
+    end 
     new_array
 end
